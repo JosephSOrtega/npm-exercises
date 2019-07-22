5904641268d2a44c55d8e7e74dbf6745a12cf714
JustinSOrtega <justinsortega@gmail.com>
2019-07-22 07:17:40 -0500

    monday morning

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
+
+const happy = require('./say-hello.js');
+//or
+// import happy from './say-hello.js'
+
+console.log(happy());
+
+
+/////////////////////////////////////Exercise Modules ^
+
+/////////////////////////////////////Exercise NPM v
+
+
 const $ = require('jquery');
 
 $("body").css("background-color", "blue");
---
+++ src/say-hello.js
@@ -0,0 +1,7 @@
+'use strict';
+const happy = () => "You can do it! Just keep yo head up!";
+
+module.exports = happy;
+
+//or
+// export default happy;
